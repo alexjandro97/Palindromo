4e8326a10fa7d4ef72f17c01166b2d3aa3f24a8b
alexj <alexj@Alex>
2017-04-23 13:45:33 +0200

    Como comprobar si la variable en la que tenemos el String es un palíndromo o no.

---
+++ src/palindromo/Palindromo.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package palindromo;
+
+/**
+ *
+ * @author alexj
+ */
+public class Palindromo {
+    
+    static String palindromo = "seres";
+    static String prueba1 = "";
+    static String prueba2 = "";
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Hacemos la comprobacion de la palabra de izquierda a derecha
+        //y la guardamos en la variable prueba1, declarada anteriormente
+        for (int i = 0; i < palindromo.length(); i++){
+            prueba1 += palindromo.charAt(i);
+        }
+        //Hacemos la comprobacion de la palabra de derecha a izquierda
+        //y la guardamos en la variable prueba2, declarada anteriormente
+        for (int j = palindromo.length()-1; j >= 0; j--){
+           prueba2 += palindromo.charAt(j);
+        }
+       
+        //comprobamos si son iguales
+        if(prueba1.equals(prueba2)){
+            System.out.println("Es un Palíndromo.");
+        } else {
+            System.out.println("No es un Palíndromo.");
+        }
+    }    
+}
