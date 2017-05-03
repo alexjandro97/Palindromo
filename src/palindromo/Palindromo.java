79570b2a1dff8a5216893beefa49657b18568251
alexj <alexj@DESKTOP-RV564FS>
2017-05-03 12:11:49 +0200

    Corregido y sin bugs

--- src/palindromo/Palindromo.java
+++ src/palindromo/Palindromo.java
@@ -11,7 +11,7 @@ package palindromo;
  */
 public class Palindromo {
     
-    static String palindromo = "seres";
+    static String palindromo = "acaso hubo buhos aca";
     static String prueba1 = "";
     static String prueba2 = "";
     
@@ -20,22 +20,35 @@ public class Palindromo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        compruebaPalindromo();
+    }    
+    
+    static private void compruebaPalindromo() {
         //Hacemos la comprobacion de la palabra de izquierda a derecha
         //y la guardamos en la variable prueba1, declarada anteriormente
-        for (int i = 0; i < palindromo.length(); i++){
-            prueba1 += palindromo.charAt(i);
+        for (int i = 0; i < palindromo.length(); i++) {
+            if (palindromo.charAt(i) != ' ') {      //se ponen comillas simple porq es un char no un string
+                prueba1 += palindromo.charAt(i);
+            }
         }
         //Hacemos la comprobacion de la palabra de derecha a izquierda
         //y la guardamos en la variable prueba2, declarada anteriormente
-        for (int j = palindromo.length()-1; j >= 0; j--){
-           prueba2 += palindromo.charAt(j);
+        for (int j = palindromo.length() - 1; j >= 0; j--) {
+            if (palindromo.charAt(j) != ' ') {
+                prueba2 += palindromo.charAt(j);
+            }
         }
        
+        prueba1.replace(" ","");
+        prueba2.replace(" ","");
+        System.out.println(prueba1);
+        System.out.println(prueba2);
+        
         //comprobamos si son iguales
         if(prueba1.equals(prueba2)){
             System.out.println("Es un Palíndromo.");
         } else {
             System.out.println("No es un Palíndromo.");
         }
-    }    
+    }
 }
